a342fe2a8bf73afe47c867f517a8f692111fbf41
Abdelrahman Abdelhamed <AbdelrhmanAbdelhamed@gmail.com>
2018-06-22 00:50:47 +0200

    cordova is not defined

--- www/googleDrive.js
+++ www/googleDrive.js
@@ -2,17 +2,36 @@ var exec = cordova.require('cordova/exec');
 
 var service = 'GoogleDrive';
 
-module.exports = {
-    signIn: function (isFileScoped, isAppFolderScoped, successCallback, errorCallback) {
+var googleDrive = {
+    signIn: function (isFileScoped, isAppFolderScoped, errorCallBack, SuccessCallBack) {
         var action = 'signIn';
-        exec(successCallback, errorCallback, service, action, [isFileScoped, isAppFolderScoped]);
+        console.error(service + " " + action + " cordova is not available");
     },
-    createFile: function (title, contents, mimeType, inAppFolder, successCallback, errorCallback) {
+    createFile: function (title, contents, mimeType, inAppFolder, errorCallBack, SuccessCallBack) {
         var action = 'createFile';
-        exec(successCallback, errorCallback, service, action, [title, contents, mimeType, inAppFolder]);
+        console.error(service + " " + action + " cordova is not available");
     },
-    retrieveFileContentsByTitle: function (title, inAppFolder, successCallback, errorCallback) {
+    retrieveFileContentsByTitle: function (title, inAppFolder, errorCallBack, SuccessCallBack) {
         var action = 'retrieveFileContentsByTitle';
-        exec(successCallback, errorCallback, service, action, [title, inAppFolder]);
+        console.error(service + " " + action + " cordova is not available");
     }
 };
+
+if (typeof cordova !== 'undefined') {
+    googleDrive = {
+        signIn: function (isFileScoped, isAppFolderScoped, successCallback, errorCallback) {
+            var action = 'signIn';
+            exec(successCallback, errorCallback, service, action, [isFileScoped, isAppFolderScoped]);
+        },
+        createFile: function (title, contents, mimeType, inAppFolder, successCallback, errorCallback) {
+            var action = 'createFile';
+            exec(successCallback, errorCallback, service, action, [title, contents, mimeType, inAppFolder]);
+        },
+        retrieveFileContentsByTitle: function (title, inAppFolder, successCallback, errorCallback) {
+            var action = 'retrieveFileContentsByTitle';
+            exec(successCallback, errorCallback, service, action, [title, inAppFolder]);
+        }
+    };
+
+}
+module.exports = googleDrive;
